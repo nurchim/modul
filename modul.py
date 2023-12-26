781a953a994b2bd3e12dd81e3e15a3bd7b5ef61b
nurchim <73731640+nurchim@users.noreply.github.com>
2023-12-27 05:26:16 +0700

    Create modul.py

---
+++ modul.py
@@ -0,0 +1,11 @@
+class OOP: 
+      
+    # methods 
+    def add(self, a, b): 
+        return a + b 
+    def sub(self, a, b): 
+        return a - b 
+  
+# explicit function       
+def method(): 
+    print("Universitas Duta Bangsa Surakarta")
