eae6c2d7e161089dc8a02b6249725e6530deeb59
nurchim <73731640+nurchim@users.noreply.github.com>
2024-01-11 14:56:19 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='modul_nurchim',
+    version='0.1',
+    description='sample pemanggilan modul secara online',
+    author='nurchim',
+    author_email='nurchim@udb.ac.id',
+    license='UDB',
+    packages=['modul_nurchim'],
+    scripts=['bin/test.py'],
+    zip_safe=False)
